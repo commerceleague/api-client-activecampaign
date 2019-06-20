7cc01feb3cc0603c72be5bd0184237eb1354a241
Andre Flitsch <andre@pixelperfect.at>
2019-06-20 12:00:12 +0200

    Add DeepData/ConnectionApi

---
+++ src/Api/DeepData/ConnectionApi.php
@@ -0,0 +1,68 @@
+<?php
+declare(strict_types=1);
+/**
+ * Copyright © André Flitsch. All rights reserved.
+ * See license.md for license details.
+ */
+
+namespace CommerceLeague\ActiveCampaignApi\Api\DeepData;
+
+use CommerceLeague\ActiveCampaignApi\Client\CommonResourceClientInterface;
+use CommerceLeague\ActiveCampaignApi\Exception\HttpException;
+use CommerceLeague\ActiveCampaignApi\Exception\InvalidArgumentException;
+
+/**
+ * Class ConnectionApi
+ *
+ * @package CommerceLeague\ActiveCampaignApi\Api\DeepData
+ */
+class ConnectionApi implements ConnectionApiResourceInterface
+{
+
+    /**
+     * @var CommonResourceClientInterface
+     */
+    private $resourceClient;
+
+    /**
+     * @param CommonResourceClientInterface $resourceClient
+     */
+    public function __construct(
+        CommonResourceClientInterface $resourceClient
+    )
+    {
+        $this->resourceClient = $resourceClient;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function create(array $data): array
+    {
+        return $this->resourceClient->createResource('api/3/connections', [], $data);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function delete(int $id): bool
+    {
+        return $this->resourceClient->deleteResource('api/3/connections/%s', [$id]);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function get(int $id): array
+    {
+        return $this->resourceClient->getResource('api/3/connections/%s', [$id]);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function update(int $id, array $data = []): array
+    {
+        return $this->resourceClient->updateResource('api/3/connections/%s', [$id], $data);
+    }
+}
