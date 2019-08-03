8cf6368e982f3b7a0b6c3b76ca95450bfe52b2bd
Andreas Penz <office@dopa.media>
2019-08-03 07:44:55 +0200

    fix phpcs issues

--- src/Api/DealApiResourceInterface.php
+++ src/Api/DealApiResourceInterface.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 
 namespace CommerceLeague\ActiveCampaignApi\Api;
 
-
 use CommerceLeague\ActiveCampaignApi\Api\Operation\CreatableResourceInterface;
 use CommerceLeague\ActiveCampaignApi\Api\Operation\DeletableResourceInterface;
 use CommerceLeague\ActiveCampaignApi\Api\Operation\GettableResourceInterface;
