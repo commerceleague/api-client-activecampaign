feac5506eae3cbfbc5f7ba647d90964cb7bc62e5
Andre Flitsch <andre@pixelperfect.at>
2020-01-13 18:07:07 +0100

    make functional test endpoint configurable

---
+++ tests/functional/Config/Loader/YamlConfigLoader.php
@@ -0,0 +1,52 @@
+<?php
+declare(strict_types=1);
+/**
+ * Copyright © André Flitsch. All rights reserved.
+ * See license.md for license details.
+ */
+
+namespace CommerceLeague\ActiveCampaignApi\tests\functional\Config\Loader;
+
+use CommerceLeague\ActiveCampaignApi\tests\functional\Config\Model\ApiConfig;
+use Symfony\Component\Config\Loader\FileLoader;
+use Symfony\Component\Yaml\Yaml;
+
+/**
+ * Class YamlConfigLoader
+ *
+ * @package CommerceLeague\ActiveCampaignApi\tests\functional\Config
+ */
+class YamlConfigLoader extends FileLoader
+{
+
+    /**
+     * Loads a resource.
+     *
+     * @param mixed $resource The resource
+     *
+     * @throws \Exception If something went wrong
+     */
+    public function load($resource, string $type = null)
+    {
+        $configValues = Yaml::parse(file_get_contents($resource));
+        $apiConfig = new ApiConfig();
+        $apiConfig->setUrl($configValues['credentials']['url'])
+            ->setToken($configValues['credentials']['token']);
+        return $apiConfig;
+    }
+
+    /**
+     * Returns whether this class supports the given resource.
+     *
+     * @param mixed $resource A resource
+     *
+     * @return bool True if this class supports the given resource, false otherwise
+     */
+    public function supports($resource, string $type = null)
+    {
+        return is_string($resource) && 'yaml' === pathinfo(
+            $resource,
+            PATHINFO_EXTENSION
+        );
+    }
+}
